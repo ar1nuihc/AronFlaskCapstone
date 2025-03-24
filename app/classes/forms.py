6c4adbaab6c7b8d25026aaa0bdd4c362c0ac2bd4
ar1nuihc <aron.chiu21@gmail.com>
2025-03-24 11:44:34 -0700

    fields cahnge

--- app/classes/forms.py
+++ app/classes/forms.py
@@ -5,7 +5,7 @@
 from flask_wtf import FlaskForm
 import mongoengine.errors
 from wtforms.validators import URL, Email, DataRequired, NumberRange
-from wtforms.fields.html5 import URLField, DateField, IntegerRangeField, EmailField
+from wtforms.fields import URLField, DateField, EmailField
 from wtforms import StringField, SubmitField, TextAreaField, IntegerField, SelectField, FileField, RadioField
 from wtforms_components import TimeField
 
